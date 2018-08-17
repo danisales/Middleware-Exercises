48dc5742dab426066151dd27b2de8a92ed0564f3
Danielle Sales <dso@cin.ufpe.br>
2018-08-17 00:16:56 -0300

    Change fork type to Lock

--- Ex01/src/com/company/Philosopher.java
+++ Ex01/src/com/company/Philosopher.java
@@ -1,41 +1,40 @@
 package com.company;
 
-
+import java.util.concurrent.locks.Lock;
 
 public class Philosopher extends Thread {
-    private PhilosopherAction action = PhilosopherAction.THINKING; //if he is not thinking he is eating
-    private int tablePosition;
-    private int[] forks;
+	private int tablePosition;
+	private Lock[] forks;
 
-    public enum PhilosopherAction {
-        THINKING,
-        EATING
-    }
+	public Philosopher(int tablePosition, Lock[] forks) {
+		this.tablePosition = tablePosition;
+		this.forks = forks;
+	}
 
-    public Philosopher(int tablePosition, int[] forks) {
-        this.tablePosition = tablePosition;
-        this.forks = forks;
-    }
+	public void threadSleep(){
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    public void run() {
-        while(true) {
-            if (forks[tablePosition] == 0 && forks[(tablePosition + 1) % forks.length] == 0) {
-                this.forks[tablePosition] = 1;
-                this.forks[(tablePosition + 1) % forks.length] = 1;
-                this.action = PhilosopherAction.EATING;
-                try {
-                    Thread.sleep(200);
-                    this.forks[tablePosition] = 0;
-                    this.forks[(tablePosition + 1) % forks.length] = 0;
-                } catch (InterruptedException e) {
-                    System.out.println(e);
-                }
-
-            } else {
-
-                this.action = PhilosopherAction.THINKING;
-            }
-        }
-    }
-
+	public void run() {
+		while(true) {
+			this.threadSleep(); // Thinking
+			
+			this.forks[tablePosition].lock();
+			System.out.println("Lock " + tablePosition + ", fork" + tablePosition);
+			this.forks[(tablePosition + 1) % forks.length].lock();
+			System.out.println("Lock " + tablePosition + ", fork" + (tablePosition + 1) % forks.length);
+			
+			this.threadSleep(); // Eating
+			
+			this.forks[tablePosition].unlock();
+			System.out.println("Unlock " + tablePosition + ", fork" + tablePosition);
+			this.forks[(tablePosition + 1) % forks.length].unlock();
+			System.out.println("Unlock " + tablePosition + ", fork" + (tablePosition + 1) % forks.length);
+		}
+	}
 }
